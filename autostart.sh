e449e9af8bf352bb614b2ab270878e051f1c31b5
biscuitrescue <karttikeyasinha11@gmail.com>
2022-06-13 01:50:43 +0530

    first

--- autostart.sh
+++ autostart.sh
@@ -5,5 +5,5 @@ mako &
 killall xfce-polkit
 xfce-polkit &
 nm-applet --indicator &
-xss-lock --transfer-sleep-lock -- wayblur --nofork &
-swaybg -i ~/Pictures/Wallpapers/ign_farFromTomorrow.png &
+killall xss-lock
+xss-lock -l wayblur &
